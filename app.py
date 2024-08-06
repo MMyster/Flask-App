b489386de7ccebfbc823758edb4bc7e543aeb332
MMyster <mouradlatoundji@gmail.com>
2024-08-06 18:22:48 +0200

    Login & Upload

--- app.py
+++ app.py
@@ -12,6 +12,26 @@ def other():
     some_text = "Example"
     return render_template('other.html', text=some_text)
 
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET' :
+        return render_template('login.html')
+    if request.method == 'POST' :
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        if username == "mlatoundji" and password=="admin":
+            return 'Success'
+        else:
+            return 'Failure'
+
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload():
+    if request.method == 'GET':
+        return render_template('upload.html')
+    if request.method == 'POST':
+        return ""
+
 @app.template_filter('reverse_string')
 def reverse_string(s):
     return s[::-1]
